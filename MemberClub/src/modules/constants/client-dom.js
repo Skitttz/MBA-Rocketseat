38ff2ca7b6fb3df4254408eb75c5318958f471c6
SKitttz <skittz.dev@gmail.com>
2024-08-08 23:28:42 -0300

    feat(MemberClub): constants dom

---
+++ MemberClub/src/modules/constants/client-dom.js
@@ -0,0 +1,9 @@
+export const username = document.querySelector('.user-name');
+export const userID = document.querySelector('.user-id span');
+export const userCreated = document.querySelector('.date-user-created');
+export const userAvatar = document.querySelector('.user-avatar');
+export const userAvatarContainer = document.querySelector(
+  '.container-image-user',
+);
+export const userHistory = document.querySelector('.history-list');
+export const userCardList = document.querySelectorAll('.card-item');
---
+++ MemberClub/src/modules/constants/progress-dom.js
@@ -0,0 +1,3 @@
+export const numbercut = document.querySelectorAll('.number-cut');
+export const userCutsRemaning = document.querySelector('.user-cuts-remaining');
+export const progressBar = document.querySelector('.actual-progress');
